f19706df78d340ed78bf4fe95c1b2eaeadd7a71a
Karim Alibhai <karim@alibhai.co>
2021-12-07 03:58:57 +0000

    add profiling

---
+++ src/profiler-sample.ts
@@ -0,0 +1,38 @@
+import Benchmark from 'benchmark'
+
+import { Type, plainToClass } from './'
+import * as classTransformer from 'class-transformer'
+
+class BenchClass {
+	@Type(() => BenchClass)
+	nested: BenchClass
+
+	@Type(() => Boolean)
+	bool: boolean
+
+	@Type(() => Date)
+	date: Date
+
+	@Type(() => Date)
+	other_date: Date
+}
+
+const benchData: BenchClass = {
+	nested: {
+		bool: null,
+		nested: {
+			bool: 'true',
+			nested: {
+				bool: false,
+				date: new Date().toISOString(),
+			},
+		},
+		other_date: new Date().toISOString(),
+	},
+	date: new Date().toISOString(),
+}
+
+for (let i = 0; i < 1e3; i++) {
+	plainToClass(BenchClass, benchData)
+}
+
